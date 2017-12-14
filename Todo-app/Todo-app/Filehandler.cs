2b48ddcfd29a338cb5405130f1de2f91cf9a57be
Endirell <endirell@gmail.com>
2017-12-14 14:08:15 +0100

    Filehandler class crated

---
+++ Todo-app/Todo-app/Filehandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Todo_app
+{
+    public class Filehandler
+    {
+        
+        public static void Reader()
+        {
+            string path = @"C:\Users\user\greenfox\Endirell-todo-app\Todo-app\todo.txt";
+
+            string[] content = File.ReadAllLines(path);
+
+            foreach (string line in content)
+            {
+                Console.WriteLine(line);
+            }
+            
+        }        
+        
+
+       
+
+    }
+}
--- Todo-app/Todo-app/Program.cs
+++ Todo-app/Todo-app/Program.cs
@@ -3,13 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Todo_app
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+            Filehandler.Reader();
+
+            Console.ReadLine();
+
+            if (args.Length == 0)
+            {
+
+                Console.WriteLine("Command Line Todo application\n=============================\n\nCommand line arguments:" +
+                "\n -l  Lists all the tasks\n -a  Adds a new task\n -r  Removes an task\n -c  Completes an task\n");
+
+            }
+           
+           
+
+            Console.ReadLine();
+
+
+
         }
+        
     }
 }
